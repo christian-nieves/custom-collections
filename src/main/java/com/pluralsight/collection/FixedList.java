d3ade83f7c45d823784c5df6bc81bf02d546c3d6
christian-nieves <thechristiannieves@gmail.com>
2026-05-18 12:49:19 -0400

    add method done

--- src/main/java/com/pluralsight/collection/FixedList.java
+++ src/main/java/com/pluralsight/collection/FixedList.java
@@ -1,4 +1,26 @@
 package com.pluralsight.collection;
 
-public class FixedList {
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FixedList<T> {
+    private List<T> items;
+    private int maxSize;
+
+    public FixedList(int maxSize) {
+        this.items = new ArrayList<>();
+        this.maxSize = maxSize;
+    }
+
+    public void add(T item) {
+        if (items.size() < maxSize) {
+            items.add(item);
+        }
+    }
+
+
+
 }
+
+
